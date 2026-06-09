96dece3046aa9449c03cf6a0b266e4402aaf09bf
Mehedi By Moni <anikkhan6464@gmail.com>
2026-06-09 15:26:45 +0600

    dlt

--- Script.js
+++
@@ -1,9 +0,0 @@
-function orderNow(productName) {
-    let phone = "8801929174650"; // তোমার নাম্বার (0 বাদ দিয়ে)
-    
-    let message = `আসসালামু আলাইকুম, আমি ${productName} অর্ডার করতে চাই। বিস্তারিত জানাবেন।`;
-    
-    let url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-    
-    window.open(url, "_blank");
-}
